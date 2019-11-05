e5bcef6cc21014628c10c40af07a792c15d449e5
Danny Kim <dudkny@gmail.com>
2019-11-05 13:58:35 -0600

    Updates

--- precache-manifest.3bcf7cc6eb9e58e61584e8c5b052bf73.js
+++ precache-manifest.3bcf7cc6eb9e58e61584e8c5b052bf73.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a810e0cffd6575937ff3d3d57e997a98",
+    "revision": "9b0d7b393859475037835622d435cf42",
     "url": "/clicky-game/index.html"
   },
   {
-    "revision": "1b9444817fdd7068a67c",
-    "url": "/clicky-game/static/css/main.755f37cf.chunk.css"
+    "revision": "be494f5b3ad3fc1638a0",
+    "url": "/clicky-game/static/css/main.caa3b5db.chunk.css"
   },
   {
     "revision": "8ddf23b8ae8e7f5c8847",
     "url": "/clicky-game/static/js/2.19c06209.chunk.js"
   },
   {
-    "revision": "1b9444817fdd7068a67c",
-    "url": "/clicky-game/static/js/main.81c7f772.chunk.js"
+    "revision": "be494f5b3ad3fc1638a0",
+    "url": "/clicky-game/static/js/main.7a8f16a5.chunk.js"
   },
   {
     "revision": "9e63f44ec9a3dfc4c648",
